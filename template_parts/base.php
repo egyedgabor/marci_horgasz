07dbe41a43cfb7468d3a4218f4ac480df6e52708
Egyed Zoltán <zoltan.egyed@vertis.com>
2015-06-18 21:22:18 +0200

    remove unneseceary css files

--- template_parts/base.php
+++ template_parts/base.php
@@ -1,6 +1,20 @@
 <?php require_once "components/phpti_0.9/ti.php" ?>
 <html>
-  <?php include "template_parts/header.php" ?>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="image/icon.png">
+
+    <title>Héjja Márton Kristóf</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">    
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="css/profile_box.css" rel="stylesheet">
+    <link href="css/reviews.css" rel="stylesheet">
+  </head>
   <body class="hatter">
     <?php include "template_parts/menu.php" ?>
     <div class="container main-block">
