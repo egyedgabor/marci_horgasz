1c5ac516a821f73dc3648ceb1b41093fd2163a3a
egyedgabor <egyedg90@gmail.com>
2015-06-23 22:32:15 +0200

    menu funny

--- js/main.js
+++ js/main.js
@@ -56,17 +56,21 @@ $('#image-gallery-caption').next().css("margin-left", "-453");
 $('#image-gallery-caption').next().css("margin-top","-49");
 $('.modal-footer').children().first().css("text-align","left");
 
+
+
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
-    $('.navbar-header').hide(1000);
-    $('.sponsors').hide(1000);
-    $('.navbar').addClass('shrink');
-    $('.menu-item').hide()
-    $('.active').show()
+      $('.navbar').mouseenter(function(){
+      $('.navbar-header').show();
+      $('.navbar').removeClass('shrink');
+      $('.sponsors').show();
+      $('.menu-item').show()
+    }
   } else {
-    $('.navbar-header').show(1000);
-    $('.navbar').removeClass('shrink');
-    $('.sponsors').show(1000);
-    $('.menu-item').show()
+      $('.navbar-header').hide(1000);
+      $('.sponsors').hide(1000);
+      $('.navbar').addClass('shrink');
+      $('.menu-item').hide()
+      $('.active').show()
   }
 });
