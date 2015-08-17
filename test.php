a9ab976c401c3528cecb9b054f612032ce253627
Egyed Zoltán <zoltan.egyed@vertis.com>
2015-08-17 20:15:44 +0200

    delete unneseceary files

--- test.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <?php include "template_parts/header.php" ?>
-    <body class="hatter main">
-      <?php include "template_parts/menu.php" ?>
-        <div> HELLO
-        </div>
-      <?php include "template_parts/footer.php" ?>
-      <?php include "template_parts/js.php" ?>
-</html>
