9f298d83a3afff38ddfcef5a3cdcee8aa75c46c1
Egyed Zoltán <zoltan.egyed@vertis.com>
2015-08-17 20:54:33 +0200

    email send done

--- test_email.php
+++ test_email.php
@@ -1,27 +1,25 @@
 <?php
-  require_once "components/PHPMailer/class.phpmailer.php";
-  require_once "components/PHPMailer/class.smtp.php";
-  require_once "env.php";
+date_default_timezone_set('Etc/UTC');
+require 'components/PHPMailer/PHPMailerAutoload.php';
+require_once "env.php";
 
-  $mail = new PHPMailer();
-  $body = file_get_contents('template_parts/email_templates/contact_email.html');
-  $body = eregi_replace("[\]",'',$body);
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Debugoutput = 'html';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = $ENV["MYMAIL"];
+$mail->Password = $ENV["MYMAIL_PASSWORD"];
+$mail->setFrom($ENV["MYMAIL"]);
+$mail->AddAddress("egyedg90@gmail.com");
+$mail->Subject = 'PHPMailer GMail SMTP test';
+$mail->msgHTML(file_get_contents('template_parts/email_templates/contact_email.html'));
 
-  $mail->IsSMTP();                             // telling the class to use SMTP
-  $mail->SMTPDebug  = 2;                       //  1 = errors and messages, 2 = messages only
-  $mail->SMTPAuth   = true;                    // enable SMTP authentication
-  $mail->SMTPSecure = "tls";                   // sets the prefix to the servier
-  $mail->Host       = "smtp.gmail.com";        // sets GMAIL as the SMTP server
-  $mail->Port       = 587;                     // set the SMTP port for the GMAIL server
-  $mail->Username   = $ENV["MYMAIL"];          // GMAIL username
-  $mail->Password   = $ENV["MYMAIL_PASSWORD"]; // GMAIL password
-  $mail->Subject    = "Proba email";
-  $mail->MsgHTML($body);
-  $mail->AddAddress("egyedg90@gmail.com", "Egyed Gábor");
-
-  if(!$mail->Send()) {
+if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
-  } else {
+} else {
     echo "Message sent!";
-  }
-?>
+}
